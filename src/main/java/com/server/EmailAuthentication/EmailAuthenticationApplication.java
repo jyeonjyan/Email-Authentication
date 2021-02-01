076647b3591a0c1c3cd06d3a65f4df53598067fa
johnjihwan <s20062@gsm.hs.kr>
2021-02-02 01:56:32 +0900

    [UPDATE]I set up the initial development related to email authentication.

---
+++ src/main/java/com/server/EmailAuthentication/EmailAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.server.EmailAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/EmailAuthentication/EmailAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.EmailAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
